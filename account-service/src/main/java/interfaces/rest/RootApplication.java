792df151c4aa4522828b3e647905274f46537f40
Kasper L. Stilling <klstilling@gmail.com>
2021-01-14 15:33:59 +0100

    tidy up account service REST adapter

--- account-service/src/main/java/interfaces/rest/RootApplication.java
+++ account-service/src/main/java/interfaces/rest/RootApplication.java
@@ -1,5 +1,8 @@
 package interfaces.rest;
 
+import interfaces.rabbitmq.RabbitMQAdapter;
+import interfaces.rabbitmq.RabbitMQAdapterFactory;
+
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
@@ -27,10 +30,16 @@ public class RootApplication extends Application {
 
     void onStart(@Observes StartupEvent ev) {
         LOGGER.info("The application is starting...");
+        try {
+            RabbitMQAdapter adapter = new RabbitMQAdapterFactory().getAdapter();
+            LOGGER.log(Level.INFO, "RabbitMQ listening...");
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "RabbitMQ failed to start: " + e.getMessage());
+        }
     }
 
     void onStop(@Observes ShutdownEvent ev) {
         LOGGER.info("The application is stopping...");
     }
-
 }
