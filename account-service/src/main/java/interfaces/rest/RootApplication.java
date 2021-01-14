a04f0f94a7924d668f1ab52748679a2f5362c481
Kasper L. Stilling <klstilling@gmail.com>
2021-01-14 15:39:17 +0100

    remove unused rabbitmq references from the REST adapter

--- account-service/src/main/java/interfaces/rest/RootApplication.java
+++ account-service/src/main/java/interfaces/rest/RootApplication.java
@@ -1,8 +1,5 @@
 package interfaces.rest;
 
-import interfaces.rabbitmq.RabbitMQAdapter;
-import interfaces.rabbitmq.RabbitMQAdapterFactory;
-
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
@@ -30,13 +27,6 @@ public class RootApplication extends Application {
 
     void onStart(@Observes StartupEvent ev) {
         LOGGER.info("The application is starting...");
-        try {
-            RabbitMQAdapter adapter = new RabbitMQAdapterFactory().getAdapter();
-            LOGGER.log(Level.INFO, "RabbitMQ listening...");
-        } catch (Exception e) {
-            e.printStackTrace();
-            LOGGER.log(Level.SEVERE, "RabbitMQ failed to start: " + e.getMessage());
-        }
     }
 
     void onStop(@Observes ShutdownEvent ev) {
