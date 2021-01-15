5e20b93e3427c90d9a10dd04ed4188ca85912a03
iyyel <i@iyyel.io>
2021-01-15 20:16:17 +0100

    Removed merchant and customer services (backed up)

--- merchant-service/src/main/java/infrastructure/repositories/interfaces/IMerchantRepository.java
+++
@@ -1,9 +0,0 @@
-package infrastructure.repositories.interfaces;
-
-import domain.Merchant;
-import exceptions.MerchantException;
-
-public interface IMerchantRepository extends IRepository<Merchant> {
-    Merchant get(String id) throws MerchantException;
-    void delete(String id) throws MerchantException;
-}
