1c8e9a588e80002b4b3c93e8698c15bffa507352
Troels Lund <trolund@gmail.com>
2021-01-17 14:16:02 +0100

    fuck 2

---
+++ rest/src/main/java/interfaces/rest/TokenResource.java
@@ -0,0 +1,91 @@
+package interfaces.rest;
+
+import dto.Token;
+import exceptions.*;
+import exceptions.token.InvalidTokenException;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import services.interfaces.ITokenService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Tag(ref = "TokenResource")
+@Path("/token")
+public class TokenResource {
+
+    @Inject
+    ITokenService service;
+
+    @Tag(ref = "requestTokens")
+    @POST
+    public Response requestTokens(@QueryParam("customerId") String customerId,
+                                  @QueryParam("amount") int amount) {
+        try {
+            service.requestTokens(customerId, amount);
+            return Response
+                    .status(Response.Status.OK)
+                    .build();
+        } catch (CustomerNotFoundException | TooManyTokensException | CustomerAlreadyRegisteredException e) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+    @Tag(ref = "getToken")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{customerId}")
+    public Response getToken(@PathParam("customerId") String customerId) {
+        try {
+            Token token = service.getToken(customerId);
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(token)
+                    .build();
+        } catch (CustomerNotFoundException | CustomerHasNoTokensException e) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+    @Tag(ref = "validateToken")
+    @POST
+    @Path("validate/{tokenId}")
+    public Response validateToken(@PathParam("tokenId") String tokenId) {
+        try {
+            service.validateToken(tokenId);
+            return Response
+                    .status(Response.Status.OK)
+                    .build();
+        } catch (CustomerNotFoundException | TokenNotFoundException | InvalidTokenException e) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+    @Tag(ref = "retireCustomerTokens")
+    @DELETE
+    @Path("{customerId}")
+    public Response retireCustomerTokens(@PathParam("customerId") String customerId) {
+        try {
+            service.deleteCustomer(customerId);
+            return Response
+                    .status(Response.Status.OK)
+                    .build();
+        } catch (CustomerNotFoundException e) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+}
---
+++ rest/src/main/java/services/interfaces/PaymentEventService.java
@@ -0,0 +1,108 @@
+package services.interfaces;
+
+import dto.TransactionDTO;
+import exceptions.account.AccountNotFoundException;
+import interfaces.rest.RootApplication;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+/**
+ * @primary-author Daniel (s151641)
+ * @co-author Troels (s161791)
+ */
+public class PaymentEventService implements EventReceiver {
+
+    private final static Logger LOGGER = Logger.getLogger(RootApplication.class.getName());
+    private final EventSender eventSender;
+    TransactionService ts;
+    private CompletableFuture<String> result;
+
+    public PaymentEventService(EventSender eventSender) {
+        this.eventSender = eventSender;
+        this.ts = ts;
+    }
+
+    @Override
+    public void receiveEvent(Event event) {
+        switch (event.getEventType()) {
+            case "getLatestTransaction":
+                getLatestTransaction(event);
+                break;
+            case "getTransactions":
+                getTransactions(event);
+                break;
+            default:
+                LOGGER.log(Level.WARNING, "Ignored event with type: " + event.getEventType() + ". Event: " + event.toString());
+                break;
+        }
+    }
+
+    public void sendTransactionDone(TransactionDTO dto, boolean successful) throws Exception {
+        Event event = null;
+        if (successful) {
+            event = new Event("TransactionSuccessful", new Object[]{dto});
+        } else {
+            event = new Event("TransactionFailed", new Object[]{dto});
+        }
+        eventSender.sendEvent(event);
+    }
+
+    private void getLatestTransaction(Event event) {
+        Object[] arguments;
+        Event eventToSend;
+
+        String accountId = (String) event.getArguments()[0];
+
+        try {
+            arguments = new Object[]{ts.getLatestTransaction(accountId)};
+            eventToSend = new Event("getLatestTransactionSuccessful", arguments);
+        } catch (Exception e) {
+            e.printStackTrace();
+            eventToSend = new Event("getLatestTransactionFailed");
+        }
+
+        try {
+            eventSender.sendEvent(eventToSend);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.WARNING, "Event send failed: " + e.getMessage());
+        }
+    }
+
+    private void getTransactions(Event event) {
+        Object[] arguments;
+        Event eventToSend;
+
+        String accountId = (String) event.getArguments()[0];
+
+        try {
+            List<TransactionDTO> dtos = ts.getTransactions(accountId);
+
+            List<TransactionDTO> dtosMapped = dtos.stream().peek(t -> {
+
+                String[] tokenarray = t.getDescription().split(" ");
+                String token = tokenarray[tokenarray.length - 1];
+
+                t.setToken(token);
+
+            }).collect(Collectors.toList());
+            arguments = new Object[]{dtosMapped};
+            eventToSend = new Event("getAllTransactionsSuccessful", arguments);
+        } catch (AccountNotFoundException e) {
+            e.printStackTrace();
+            eventToSend = new Event("getAllTransactionsFailed");
+        }
+
+        try {
+            eventSender.sendEvent(eventToSend);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.WARNING, "Event send failed: " + e.getMessage());
+        }
+    }
+
+}
