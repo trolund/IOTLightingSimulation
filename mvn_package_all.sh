06dca276eb2f62cbda656d4757c156aba91b6402
iyyel <i@iyyel.io>
2021-01-19 09:07:28 +0100

    Revert latest commit. Wasn't necessary.

--- mvn_package_all.sh
+++ mvn_package_all.sh
@@ -1,19 +1,10 @@
 # author Troels (s161791)
 # author Daniel (s151641)
 
-
-#mvn -f utils/pom.xml -D maven.test.skip=true clean install
-#mvn -f rest/pom.xml -D maven.test.skip=true package
-#mvn -f payment-service/pom.xml -D maven.test.skip=true package
-#mvn -f token-service/pom.xml -D maven.test.skip=true package
-#mvn -f account-service/pom.xml -D maven.test.skip=true package
-#mvn -f report-service/pom.xml -D maven.test.skip=true package
-#mvn -f client/pom.xml -D maven.test.skip=true package
-
 mvn -f utils/pom.xml -D maven.test.skip=true clean install
 mvn -f rest/pom.xml -D maven.test.skip=true package
-mvn -f payment-service/pom.xml package
-mvn -f token-service/pom.xml package
-mvn -f account-service/pom.xml package
-mvn -f report-service/pom.xml package
+mvn -f payment-service/pom.xml -D maven.test.skip=true package
+mvn -f token-service/pom.xml -D maven.test.skip=true package
+mvn -f account-service/pom.xml -D maven.test.skip=true package
+mvn -f report-service/pom.xml -D maven.test.skip=true package
 mvn -f client/pom.xml -D maven.test.skip=true package
